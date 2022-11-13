c27ebb00a4b90bb6f3bfe666d81f661c2c065b85
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-11-13 18:27:55 +0100

    Update barokkos.js

--- barokkos.js
+++ barokkos.js
@@ -1,9 +1,9 @@
 window.addEventListener(('load'), () => {
     if (document.querySelector('#heading') !== null) {
       window.sessionStorage.setItem('Heading', 'displayed');
-    }
-  })
+}
+})
 
-  if (window.sessionStorage.getItem('Heading')) {
-    document.querySelector('#heading').classList.remove('only_once')
-  }
+    if (window.sessionStorage.getItem('Heading')) {
+        document.querySelector('#heading').classList.remove('only_once')
+}

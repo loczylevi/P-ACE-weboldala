ffeed1a9275397e1516894fa5541b39ffdc8c009
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-02-05 10:36:29 +0100

    Add files via upload

--- barokkos.js
+++ barokkos.js
@@ -24,3 +24,5 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
---
+++ clickk_figyelo.js
@@ -0,0 +1,31 @@
+let input = document.getElementById("gomb1");
+let input2 = document.getElementById("gomb2");
+let input3 = document.getElementById("gomb3");
+let input4 = document.getElementById("gomb4");
+let input5 = document.getElementById("gomb5");
+let input6 = document.getElementById("gomb6");
+let fogomb = document.getElementById("fogomb");
+
+input.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
+
+input2.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
+
+input3.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
+
+input4.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
+
+input5.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
+
+input6.addEventListener("click", (e) => {
+  document.getElementById("fogomb").click();
+});
